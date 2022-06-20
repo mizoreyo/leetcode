27919abc6f90ec6a967094bbbdd447467c1dc275
mizoreyo <mizoreyo@outlook.com>
2022-06-20 16:21:58 +0800

    FindFirstAndLastPositionOfElementInSortedArray

--- src/leetcode/editor/cn/FindFirstAndLastPositionOfElementInSortedArray.java
+++ src/leetcode/editor/cn/FindFirstAndLastPositionOfElementInSortedArray.java
@@ -44,15 +44,51 @@ package leetcode.editor.cn;
 public class FindFirstAndLastPositionOfElementInSortedArray {
     public static void main(String[] args) {
         Solution solution = new FindFirstAndLastPositionOfElementInSortedArray().new Solution();
+        solution.searchRange(new int[]{5,7,7,8,8,10},6);
     }
     //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int n=nums.length;
-        int start=0;
-        int end=n-1;
+        if(nums.length==0||target<nums[0]||target>nums[nums.length-1]) {
+            return new int[]{-1,-1};
+        }
+        int left=getLeft(nums,target);
+        int right=getRight(nums,target);
+        return new int[]{left,right};
+    }
 
-        return null;
+    public int getLeft(int[] nums,int target) {
+        int left=0;
+        int right=nums.length-1;
+        int m;
+        while (left<=right) {
+            m=(left+right)/2;
+            if(nums[m]==target&&(m-1==-1||nums[m-1]!=target)) {
+                return m;
+            } else if(target<=nums[m]) {
+                right=m-1;
+            } else {
+                left=m+1;
+            }
+        }
+        return -1;
+    }
+
+    public int getRight(int[] nums,int target) {
+        int left=0;
+        int right=nums.length-1;
+        int m;
+        while (left<=right) {
+            m=(left+right)/2;
+            if(nums[m]==target&&(m+1== nums.length||nums[m+1]!=target)) {
+                return m;
+            } else if(target<nums[m]) {
+                right=m-1;
+            } else {
+                left=m+1;
+            }
+        }
+        return -1;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
