520b12025dbe5dfa86217f22aa4467260b853280
mizoreyo <mizoreyo@outlook.com>
2022-07-01 16:06:15 +0800

    BinaryTreePostorderTraversal Stack

--- src/leetcode/editor/cn/BinaryTreePostorderTraversal.java
+++ src/leetcode/editor/cn/BinaryTreePostorderTraversal.java
@@ -63,6 +63,9 @@ public class BinaryTreePostorderTraversal {
     class Solution {
         public List<Integer> postorderTraversal(TreeNode root) {
             List<Integer> ans=new ArrayList<>();
+            if(root==null) {
+                return ans;
+            }
             Deque<TreeNode> stack=new ArrayDeque<>();
             stack.push(root);
             while (!stack.isEmpty()) {
