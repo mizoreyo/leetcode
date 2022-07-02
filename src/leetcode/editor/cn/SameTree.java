d7cfe8f81d97cd1b5c41c176a3da8d7f8d839fe1
mizoreyo <mizoreyo@outlook.com>
2022-07-02 14:40:28 +0800

    SameTree

---
+++ src/leetcode/editor/cn/SameTree.java
@@ -0,0 +1,80 @@
+//给你两棵二叉树的根节点 p 和 q ，编写一个函数来检验这两棵树是否相同。 
+//
+// 如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：p = [1,2,3], q = [1,2,3]
+//输出：true
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：p = [1,2], q = [1,null,2]
+//输出：false
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：p = [1,2,1], q = [1,1,2]
+//输出：false
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 两棵树上的节点数目都在范围 [0, 100] 内 
+// -10⁴ <= Node.val <= 10⁴ 
+// 
+// Related Topics 树 深度优先搜索 广度优先搜索 二叉树 👍 858 👎 0
+
+package leetcode.editor.cn;
+public class SameTree {
+    public static void main(String[] args) {
+        Solution solution = new SameTree().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    class Solution {
+        public boolean isSameTree(TreeNode p, TreeNode q) {
+            return compare(p,q);
+        }
+
+        public boolean compare(TreeNode left, TreeNode right) {
+            if(left==null&&right!=null)
+                return false;
+            else if(left!=null&&right==null)
+                return false;
+            else if(left==null&&right==null)
+                return true;
+            else if(left.val!=right.val)
+                return false;
+            boolean outside=compare(left.left,right.left);
+            boolean inside=compare(left.right,right.right);
+            return outside&&inside;
+
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
