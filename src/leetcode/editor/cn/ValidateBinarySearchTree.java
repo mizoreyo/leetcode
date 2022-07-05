78c1426ea056d49c0990392e61e9737c9a3014da
mizoreyo <mizoreyo@outlook.com>
2022-07-05 21:03:32 +0800

    MergeTwoBinaryTrees and ValidateBinarySearchTree

---
+++ src/leetcode/editor/cn/ValidateBinarySearchTree.java
@@ -0,0 +1,88 @@
+//给你一个二叉树的根节点 root ，判断其是否是一个有效的二叉搜索树。 
+//
+// 有效 二叉搜索树定义如下： 
+//
+// 
+// 节点的左子树只包含 小于 当前节点的数。 
+// 节点的右子树只包含 大于 当前节点的数。 
+// 所有左子树和右子树自身必须也是二叉搜索树。 
+// 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：root = [2,1,3]
+//输出：true
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：root = [5,1,4,null,null,3,6]
+//输出：false
+//解释：根节点的值是 5 ，但是右子节点的值是 4 。
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中节点数目范围在[1, 10⁴] 内 
+// -2³¹ <= Node.val <= 2³¹ - 1 
+// 
+// Related Topics 树 深度优先搜索 二叉搜索树 二叉树 👍 1650 👎 0
+
+package leetcode.editor.cn;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidateBinarySearchTree {
+    public static void main(String[] args) {
+        Solution solution = new ValidateBinarySearchTree().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    class Solution {
+        public boolean isValidBST(TreeNode root) {
+            List<Integer> sequence=new ArrayList<>();
+            traversal(root,sequence);
+            int last=sequence.get(0);
+            int i=1;
+            while (i<sequence.size()) {
+                if(sequence.get(i)<=last)
+                    return false;
+                last=sequence.get(i);
+                i++;
+            }
+            return true;
+        }
+
+        public void traversal(TreeNode node, List<Integer> sequence) {
+            if(node==null)
+                return;
+            traversal(node.left,sequence);
+            sequence.add(node.val);
+            traversal(node.right,sequence);
+        }
+    }
+
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
