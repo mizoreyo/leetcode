75bfbd7a261faf87d020177a0b21257b0b21b195
mizoreyo <mizoreyo@outlook.com>
2022-07-24 11:33:36 +0800

    TrappingRainWater

---
+++ src/leetcode/editor/cn/TrappingRainWater.java
@@ -0,0 +1,67 @@
+//给定 n 个非负整数表示每个宽度为 1 的柱子的高度图，计算按此排列的柱子，下雨之后能接多少雨水。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//
+// 
+//输入：height = [0,1,0,2,1,0,1,3,2,1,2,1]
+//输出：6
+//解释：上面是由数组 [0,1,0,2,1,0,1,3,2,1,2,1] 表示的高度图，在这种情况下，可以接 6 个单位的雨水（蓝色部分表示雨水）。 
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：height = [4,2,0,3,2,5]
+//输出：9
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// n == height.length 
+// 1 <= n <= 2 * 10⁴ 
+// 0 <= height[i] <= 10⁵ 
+// 
+// Related Topics 栈 数组 双指针 动态规划 单调栈 👍 3631 👎 0
+
+package leetcode.editor.cn;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class TrappingRainWater {
+    public static void main(String[] args) {
+        Solution solution = new TrappingRainWater().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int trap(int[] height) {
+        int area=0;
+        Deque<Integer> stack=new ArrayDeque<>();
+        for(int i=0;i<height.length;i++) {
+            while (!stack.isEmpty()&&height[i]>height[stack.peek()]) {
+                //获取底部高度
+                int bottom=height[stack.pop()];
+                //获取左侧坐标和高度
+                if(!stack.isEmpty()) {
+                    int leftIndex=stack.peek();
+                    int left=height[leftIndex];
+                    //水深为左右取最小高度减去底部高度
+                    int depth=Math.min(left,height[i])-bottom;
+                    area+=depth*(i-leftIndex-1);
+                }
+            }
+            stack.push(i);
+        }
+        return area;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
